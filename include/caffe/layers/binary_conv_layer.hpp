e65c578ad5e2a4f4f908feeed9062b11a60742e9
zzqiuzz <zzqiuzz>
2018-05-23 13:25:15 +0800

    add missing modules and several files

--- include/caffe/layers/binary_conv_layer.hpp
+++ include/caffe/layers/binary_conv_layer.hpp
@@ -8,44 +8,54 @@
 //#include "caffe/common.hpp"
 #include "caffe/layers/base_conv_layer.hpp"
 namespace caffe{
-	template <typename Dtype>
-	class BinaryConvolutionLayer : public BaseConvolutionLayer<Dtype> {
-	public:
-		explicit BinaryConvolutionLayer(const LayerParameter& param)
-			: BaseConvolutionLayer<Dtype>(param){}
+template <typename Dtype>
+class BinaryConvolutionLayer : public BaseConvolutionLayer<Dtype> {
+public:
+	explicit BinaryConvolutionLayer(const LayerParameter& param)
+		: BaseConvolutionLayer<Dtype>(param){}
 
-		virtual inline const char* type() const { return "BinaryConvolution"; }
+	virtual inline const char* type() const { return "BinaryConvolution"; }
 
-	protected:
-		//W_b store binarized weights
-		//W_buffer store original weights.
-		shared_ptr<Blob<Dtype> > W_b;
-		shared_ptr<Blob<Dtype> > W_buffer;
-		vector<Dtype> filterMean;
-		vector<Dtype> Alpha;
-		virtual void Forward_cpu(const vector<Blob<Dtype>*>& bottom,
-			const vector<Blob<Dtype>*>& top);
-		virtual void Backward_cpu(const vector<Blob<Dtype>*>& top,
-			const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
-		virtual void compute_output_shape();
-		virtual inline bool reverse_dimensions() { return false; }
-		/*user' function e.g binary/hardsigmoid*/
-		void customConvInit();
-		virtual void meanClampBinarizeConvParam(const shared_ptr<Blob<Dtype> > weights, const shared_ptr<Blob<Dtype> > wb);
-		virtual inline void copyFromTo(const shared_ptr<Blob<Dtype> > orig, const shared_ptr<Blob<Dtype> > buf){
-			CHECK_EQ(orig->count(), buf->count());
-			caffe_copy(orig->count(), orig->cpu_data(), buf->mutable_cpu_data());
-		}
-		virtual inline Dtype clampWeights(Dtype w){
-			return w = w < -1 ? -1 : w >1 ? 1 : w;
-		}
-		virtual inline Dtype signWeights(Dtype w){
-			return w = w >= 0 ? 1 : -1;
-		}
+protected:
+	//W_b store binarized weights
+	//W_buffer store original weights.
+	shared_ptr<Blob<Dtype> > W_b;
+	shared_ptr<Blob<Dtype> > W_buffer;
+	vector<Dtype> filterMean;
+	vector<Dtype> Alpha;
+	virtual void Forward_cpu(const vector<Blob<Dtype>*>& bottom,
+		const vector<Blob<Dtype>*>& top);
+	virtual void Forward_gpu(const vector<Blob<Dtype>*>& bottom,
+		const vector<Blob<Dtype>*>& top);
+	virtual void Backward_cpu(const vector<Blob<Dtype>*>& top,
+		const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
+	virtual void Backward_gpu(const vector<Blob<Dtype>*>& top,
+		const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
+	virtual void compute_output_shape();
+	virtual inline bool reverse_dimensions() { return false; }
+	/*user' function e.g binary/hardsigmoid*/
+	void customConvInit();
+	virtual void meanClampBinarizeConvParam(const shared_ptr<Blob<Dtype> > weights, const shared_ptr<Blob<Dtype> > wb);
+	virtual inline void copyFromTo(const shared_ptr<Blob<Dtype> > orig, const shared_ptr<Blob<Dtype> > buf){
+		CHECK_EQ(orig->count(), buf->count());
+		
+#ifndef CPU_ONLY
+		caffe_copy(orig->count(), orig->gpu_data(), buf->mutable_gpu_data());
+#else
+		caffe_copy(orig->count(), orig->cpu_data(), buf->mutable_cpu_data());
+#endif
+
+	}
+	virtual inline Dtype clampWeights(Dtype w){
+		return w = w < -1 ? -1 : w >1 ? 1 : w;
+	}
+	virtual inline Dtype signWeights(Dtype w){
+		return w = w >= 0 ? 1 : -1;
+	}
 
 
 
-	};
+};
 
 }
 
--- src/caffe/layers/binary_conv_layer.cpp
+++ src/caffe/layers/binary_conv_layer.cpp
@@ -2,7 +2,7 @@
 #include "caffe/layers/binary_conv_layer.hpp"
 
 namespace caffe{
-	 
+
 template <typename Dtype>
 void BinaryConvolutionLayer<Dtype>::compute_output_shape() {
 	const int* kernel_shape_data = this->kernel_shape_.cpu_data();
@@ -32,7 +32,7 @@ void BinaryConvolutionLayer<Dtype>::customConvInit(){
 		filterMean.push_back(0);
 		Alpha.push_back(0);
 	}
-	
+
 	W_b = shared_ptr<Blob<Dtype> >(new Blob<Dtype>());
 	W_buffer = shared_ptr<Blob<Dtype> >(new Blob<Dtype>());
 	W_b->ReshapeLike(*(this->blobs_[0]));
@@ -40,7 +40,7 @@ void BinaryConvolutionLayer<Dtype>::customConvInit(){
 }
 template <typename Dtype>
 void BinaryConvolutionLayer<Dtype>::meanClampBinarizeConvParam(const shared_ptr<Blob<Dtype> > weights,
-	const shared_ptr<Blob<Dtype> > wb){ 
+	const shared_ptr<Blob<Dtype> > wb){
 	int weightsNum = weights->count();
 	int num = weights->num();
 	int channel = weights->channels();
@@ -50,13 +50,13 @@ void BinaryConvolutionLayer<Dtype>::meanClampBinarizeConvParam(const shared_ptr<
 	for (int n = 0; n < num; n++){
 		for (int c = 0; c < channel; c++){
 			for (int h = 0; h < height; h++){
-				for (int w = 0; w < width; w++){ 
+				for (int w = 0; w < width; w++){
 					filterMean[n] += weights->data_at(n, c, h, w);
-					Alpha[n] += std::abs(weights->data_at(n, c, h, w)) / Dtype(div); 
+					Alpha[n] += std::abs(weights->data_at(n, c, h, w)) / Dtype(div);
 				}
 			}
 		}
-	} 
+	}
 	for (int id = 0; id < weightsNum; id++){
 		const int num = id / div;//for each filter in the layer. 
 		wb->mutable_cpu_data()[id] = Alpha[num] * signWeights(clampWeights(weights->cpu_data()[id] - filterMean[num]));
@@ -95,41 +95,41 @@ template <typename Dtype>
 void BinaryConvolutionLayer<Dtype>::Backward_cpu(const vector<Blob<Dtype>*>& bottom, const vector<bool>& propagate_down,
 	const vector<Blob<Dtype>*>& top){
 	const Dtype* weight = this->blobs_[0]->cpu_data();
-  Dtype* weight_diff = this->blobs_[0]->mutable_cpu_diff();
-  for (int i = 0; i < top.size(); ++i) {
-    const Dtype* top_diff = top[i]->cpu_diff();
-    const Dtype* bottom_data = bottom[i]->cpu_data();
-    Dtype* bottom_diff = bottom[i]->mutable_cpu_diff();
-    // Bias gradient, if necessary.
-    if (this->bias_term_ && this->param_propagate_down_[1]) {
-      Dtype* bias_diff = this->blobs_[1]->mutable_cpu_diff();
-      for (int n = 0; n < this->num_; ++n) {
-        this->backward_cpu_bias(bias_diff, top_diff + n * this->top_dim_);
-      }
-    }
-    if (this->param_propagate_down_[0] || propagate_down[i]) {
-      for (int n = 0; n < this->num_; ++n) {
-        // gradient w.r.t. weight. Note that we will accumulate diffs.
-        if (this->param_propagate_down_[0]) {
-          this->weight_cpu_gemm(bottom_data + n * this->bottom_dim_,
-              top_diff + n * this->top_dim_, weight_diff);
-        }
-        // gradient w.r.t. bottom data, if necessary.
-        if (propagate_down[i]) {
-          this->backward_cpu_gemm(top_diff + n * this->top_dim_, weight,
-              bottom_diff + n * this->bottom_dim_);
-        }
-      }
-    }
-  }
-  copyFromTo(W_buffer,this->blobs_[0]);
+	Dtype* weight_diff = this->blobs_[0]->mutable_cpu_diff();
+	for (int i = 0; i < top.size(); ++i) {
+		const Dtype* top_diff = top[i]->cpu_diff();
+		const Dtype* bottom_data = bottom[i]->cpu_data();
+		Dtype* bottom_diff = bottom[i]->mutable_cpu_diff();
+		// Bias gradient, if necessary.
+		if (this->bias_term_ && this->param_propagate_down_[1]) {
+			Dtype* bias_diff = this->blobs_[1]->mutable_cpu_diff();
+			for (int n = 0; n < this->num_; ++n) {
+				this->backward_cpu_bias(bias_diff, top_diff + n * this->top_dim_);
+			}
+		}
+		if (this->param_propagate_down_[0] || propagate_down[i]) {
+			for (int n = 0; n < this->num_; ++n) {
+				// gradient w.r.t. weight. Note that we will accumulate diffs.
+				if (this->param_propagate_down_[0]) {
+					this->weight_cpu_gemm(bottom_data + n * this->bottom_dim_,
+						top_diff + n * this->top_dim_, weight_diff);
+				}
+				// gradient w.r.t. bottom data, if necessary.
+				if (propagate_down[i]) {
+					this->backward_cpu_gemm(top_diff + n * this->top_dim_, weight,
+						bottom_diff + n * this->bottom_dim_);
+				}
+			}
+		}
+	}
+	copyFromTo(W_buffer, this->blobs_[0]);
 
 }
 
-	/*#ifdef CPU_ONLY
-	STUB_GPU(BinaryConvolutionLayer);
-	#endif*/
+#ifdef CPU_ONLY
+STUB_GPU(BinaryConvolutionLayer);
+#endif
 
-	INSTANTIATE_CLASS(BinaryConvolutionLayer); 
-	REGISTER_LAYER_CLASS(BinaryConvolution);
+INSTANTIATE_CLASS(BinaryConvolutionLayer);
+REGISTER_LAYER_CLASS(BinaryConvolution);
 }
